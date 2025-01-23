46bdddf6d3c88b350c9e2b9e86931896a160220c
yumachin <curryuma925@gmail.com>
2025-01-23 11:47:27 +0900

    create backend with RoR

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,48 @@
+# rails g controller Api::V1::Posts: ApiディレクトリのV1ディレクトリにPostsコントローラーを作成
+
+class Api::V1::PostsController < ApplicationController
+  # リソースすべてを一覧表示
+  def index
+    # @posts: postsに対して、何か操作を加えるときに@を付けて、インスタンス変数とする
+    # Post: modelの名前
+    @posts = Post.all
+    # render: Railsのレスポンスをクライアント側に返す
+    # json: JSON形式に変更(apiが絡むときは、JSONに直す)
+    render json: @posts
+  end
+
+  # 特定のリソースを詳細表示
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :umprocessable_entity # 422(バリデーションエラー)
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+  private
+  # ストロングパラメーターを使用するとき:title(シンボル)を利用する
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
